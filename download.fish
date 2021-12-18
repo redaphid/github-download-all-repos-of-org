efed3288719dd83e4700af5cf77cd310859356b6
redaphid <iam@aaronherres.com>
2021-12-18 16:54:33 -0700

    early return guard clause. usage printing

--- download.fish
+++ download.fish
@@ -1,8 +1,13 @@
-
+#!/usr/bin/env fish
 # You need:
 # - fish (https://fishshell.com/) (You don't need to use it as your shell, but it's a good shellscripting language)
 # - gh - github cli (https://cli.github.com)
 #	- jq (https://stedolan.github.io/jq/)
+
+set -q ORG[1]; or begin
+	echo "I need the org name! ex ORG=octoblu ./download.fish" >&2
+	exit 1
+end
 
 set repos (gh repo list $ORG --limit 9999 --json name)
 set repos_to_clone (echo $repos | jq -r ".[].name")
