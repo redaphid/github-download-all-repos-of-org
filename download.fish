24b257ab0931c99326928e16cd71249ecfdd0a25
redaphid <iam@aaronherres.com>
2021-12-18 16:26:08 -0700

    get all the repos in json afaik and pipe to result json.

--- download.fish
+++ download.fish
@@ -1,25 +1,2 @@
 #!/usr/bin/env fish
-
-#ex:
-# ORG_NAME=octoblu ./download.fish
-
-set org_id (curl -H Authorization: $GITHUB_TOKEN  https://api.github.com/orgs/$ORG_NAME | jq .id)
-echo $org_id
-
-set query """
-    query($endCursor: String) {
-      viewer {
-        repositories(first: 100, after: $endCursor) {
-          nodes { organizationId $org_id }
-          pageInfo {
-            hasNextPage
-            endCursor
-          }
-        }
-      }
-    }
-"""
-
-echo $query
-
-gh api graphql --paginate -f query=$query | jq .
+echo (gh repo list octoblu --limit 9999 --json name,url) >> repos.json
