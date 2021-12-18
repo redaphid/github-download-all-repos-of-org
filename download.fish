8477d958ef8de7cc505c74ae8b4f7a6e341a921e
redaphid <iam@aaronherres.com>
2021-12-18 16:49:31 -0700

    simplest way for me to do it locally - but requires gh, fish, and jq

--- download.fish
+++ download.fish
@@ -1,10 +1,17 @@
-#!/usr/bin/env fish
-set repos (gh repo list octoblu --limit 9999 --json name,url)
 
-set urls_to_clone (echo $repos | jq -r ".[].url")
+# You need:
+# - fish (https://fishshell.com/) (You don't need to use it as your shell, but it's a good shellscripting language)
+# - gh - github cli (https://cli.github.com)
+#	- jq (https://stedolan.github.io/jq/)
 
-for u in $urls_to_clone
-	echo $u
+set repos (gh repo list $ORG --limit 9999 --json name)
+set repos_to_clone (echo $repos | jq -r ".[].name")
+
+echo "cloning repos:\n $repos_to_clone"
+for u in $repos_to_clone
+	echo "cloning $u"
+	git clone https://github.com/$ORG/$u repos/$u; or echo "failed to clone $u"
+	sleep 5
 end
 
 # echo $repos
